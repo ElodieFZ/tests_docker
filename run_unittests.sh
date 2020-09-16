6b9bcf1b0d2c7551e2c65cd2d0e16f9a74ca3b9b
Elodie Fernandez <elodief@met.no>
2020-09-16 10:55:39 +0200

    Add coverage

--- run_unittests.sh
+++ run_unittests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nosetests
+##nosetests
 
 # The following is to have it work with codecov, except it doesn't work..
-##nosetests --with-coverage --cover-package=py_mmd_tools
+nosetests --with-coverage --cover-package=my_module
 
 #if [[ -n "$CODECOV_TOKEN" ]]; then
 #  bash <(curl -s https://codecov.io/bash)
