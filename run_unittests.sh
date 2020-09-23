8efb032e539045a2880637b6beaaa8b05119e352
Elodie Fernandez <elodief@met.no>
2020-09-23 14:31:30 +0200

    Check

--- run_unittests.sh
+++ run_unittests.sh
@@ -3,7 +3,9 @@
 ##nosetests
 
 # The following is to have it work with codecov, except it doesn't work..
-nosetests --with-coverage --cover-package=my_module
+nosetests --with-coverage --cover-package=my_module || exit 1
+
+echo toto
 
 #if [[ -n "$CODECOV_TOKEN" ]]; then
 #  bash <(curl -s https://codecov.io/bash)
