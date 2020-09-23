d6ea5dbe8ee8eb92b053647b6a3a16f3c1809e8f
Elodie Fernandez <elodief@met.no>
2020-09-23 14:27:20 +0200

    Check

--- run_unittests.sh
+++ run_unittests.sh
@@ -5,8 +5,6 @@
 # The following is to have it work with codecov, except it doesn't work..
 nosetests --with-coverage --cover-package=my_module
 
-echo toto
-
 #if [[ -n "$CODECOV_TOKEN" ]]; then
 #  bash <(curl -s https://codecov.io/bash)
 #fi
